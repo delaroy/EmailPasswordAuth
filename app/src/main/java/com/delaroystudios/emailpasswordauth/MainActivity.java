091a8f760447bb381dc96f34c1cbe5823f863038
Delaroy Studios <bamoyk@yahoo.com>
2017-09-04 20:50:42 +0100

    twitterauth

--- app/src/main/java/com/delaroystudios/emailpasswordauth/MainActivity.java
+++ app/src/main/java/com/delaroystudios/emailpasswordauth/MainActivity.java
@@ -1,183 +1,120 @@
 package com.delaroystudios.emailpasswordauth;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import android.support.annotation.NonNull;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import java.io.InputStream;
+
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
 
-import java.io.InputStream;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements
-        GoogleApiClient.OnConnectionFailedListener {
+    private static final String TWITTER_KEY = "SmxH5mxnQ1mT5vJrZs9mSbJ6Y";
+    private static final String TWITTER_SECRET = "ihqB0qyBqJBxIGfaZjpePDtBWJY674irNo6eMMMwRlrCjTw6CY";
 
-private GoogleApiClient googleApiClient;
-private static final int RC_SIGN_IN = 1001;
+    private static final String TAG = "TwitterAuth";
 
-private ImageView profileImage;
-private TextView emailText;
-private TextView statusText;
-private FirebaseAuth fbAuth;
-private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth fbAuth;
+    private FirebaseAuth.AuthStateListener authListener;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+    private TwitterLoginButton loginButton;
+    private TextView userText;
+    private TextView statusText;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+
         setContentView(R.layout.activity_main);
 
-        profileImage = (ImageView) findViewById(R.id.profileImage);
-        emailText = (TextView) findViewById(R.id.emailText);
+        userText = (TextView) findViewById(R.id.userText);
         statusText = (TextView) findViewById(R.id.statusText);
+        imageView = (ImageView) findViewById(R.id.profileImage);
 
-        emailText.setText("");
-        statusText.setText("Signed Out");
+        loginButton = (TwitterLoginButton) findViewById(R.id.loginButton);
 
-        GoogleSignInOptions signInOptions =
-        new GoogleSignInOptions.Builder(
-        GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken(getString(R.string.default_web_client_id))
-        .requestEmail()
-        .requestProfile()
-        .build();
+        loginButton.setCallback(new Callback<TwitterSession>() {
 
-        googleApiClient = new GoogleApiClient.Builder(this)
-        .enableAutoManage(this, this)
-        .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
-        .build();
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d(TAG, "loginButton Callback: Success");
+                exchangeTwitterToken(result.data);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d(TAG, "loginButton Callback: Failure " +
+                        exception.getLocalizedMessage());
+            }
+        });
 
         fbAuth = FirebaseAuth.getInstance();
 
         authListener = new FirebaseAuth.AuthStateListener() {
-@Override
-public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            @Override
+            public void onAuthStateChanged(
+                    @NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    userText.setText(user.getDisplayName());
+                    statusText.setText("Signed In");
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user.getPhotoUrl() != null) {
+                        displayImage(user.getPhotoUrl());
+                    }
+                } else {
+                    userText.setText("");
+                    statusText.setText("Signed Out");
+                    imageView.setImageResource(
+                            R.drawable.tw__composer_logo_blue);
 
-        if (user != null) {
-        emailText.setText(user.getEmail());
-        statusText.setText("Signed In");
-        if (user.getPhotoUrl() != null) {
-        displayImage(user.getPhotoUrl());
-        }
-        } else {
-        emailText.setText("");
-        statusText.setText("Signed Out");
-        profileImage.setImageResource(
-        R.drawable.common_google_signin_btn_icon_dark);
-
-        }
-        }
+                }
+            }
         };
-        }
-
-public void signIn(View view) {
-        Intent signInIntent =
-        Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-        }
-
-@Override
-public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-        GoogleSignInResult result =
-        Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-
-        if (result.isSuccess()) {
-        GoogleSignInAccount account = result.getSignInAccount();
-        authWithFirebase(account);
-        } else {
-        this.notifyUser("Google sign-in failed.");
-        }
-        }
-        }
-
-private void authWithFirebase(GoogleSignInAccount acct) {
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(
-        acct.getIdToken(), null);
-
-        fbAuth.signInWithCredential(credential)
-        .addOnCompleteListener(this,
-        new OnCompleteListener<AuthResult>() {
-@Override
-public void onComplete(@NonNull Task<AuthResult> task) {
-        if (!task.isSuccessful()) {
-        notifyUser("Firebase authentication failed.");
-        }
-        }
-        });
-        }
-
-public void signOut(View view) {
-        fbAuth.signOut();
-
-        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
-        new ResultCallback<Status>() {
-@Override
-public void onResult(@NonNull Status status) {
-
-        }
-        });
-        }
-@Override
-public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        notifyUser("Google Play Services failure.");
-        }
-
-        void displayImage(Uri imageUrl) {
-        new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
-        .execute(imageUrl.toString());
-        }
-
-private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-    ImageView bmImage;
-
-    public DownloadImageTask(ImageView bmImage) {
-        this.bmImage = bmImage;
     }
-
-    protected Bitmap doInBackground(String... urls) {
-        String urldisplay = urls[0];
-        Bitmap bitmap = null;
-        try {
-            InputStream in = new java.net.URL(urldisplay).openStream();
-            bitmap = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-
-    protected void onPostExecute(Bitmap result) {
-        bmImage.setImageBitmap(result);
-    }
-}
 
     @Override
     public void onStart() {
@@ -193,8 +130,79 @@ private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         }
     }
 
-    private void notifyUser(String message) {
-        Toast.makeText(MainActivity.this, message,
-                Toast.LENGTH_SHORT).show();
+    private void exchangeTwitterToken(TwitterSession session) {
+
+        TwitterAuthClient authClient = new TwitterAuthClient();
+        authClient.requestEmail(session, new Callback<String>() {
+            @Override
+            public void success(Result<String> result) {
+                Log.d(TAG, "EMAIL = " + result.data);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        fbAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential",
+                                    task.getException());
+                            Toast.makeText(MainActivity.this,
+                                    "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+    public void signOut(View view) {
+        fbAuth.signOut();
+        //Twitter.
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void displayImage(Uri imageUrl) {
+        new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
+                .execute(imageUrl.toString());
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
 }
