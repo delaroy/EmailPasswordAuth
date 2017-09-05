89b473f07e975abe3820104113ff9502ac5fbe07
Delaroy Studios <bamoyk@yahoo.com>
2017-09-05 18:23:29 +0100

    phoneauthentication

--- app/src/main/java/com/delaroystudios/emailpasswordauth/MainActivity.java
+++ app/src/main/java/com/delaroystudios/emailpasswordauth/MainActivity.java
@@ -3,156 +3,177 @@ package com.delaroystudios.emailpasswordauth;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView userText;
+
+    private static final String TAG = "PhoneAuth";
+
+    private EditText phoneText;
+    private EditText codeText;
+    private Button verifyButton;
+    private Button sendButton;
+    private Button resendButton;
+    private Button signoutButton;
     private TextView statusText;
-    private EditText emailText;
-    private EditText passwordText;
+
+    private String phoneVerificationId;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            verificationCallbacks;
+    private PhoneAuthProvider.ForceResendingToken resendToken;
 
     private FirebaseAuth fbAuth;
-    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userText = (TextView) findViewById(R.id.userText);
+        phoneText = (EditText) findViewById(R.id.phoneText);
+        codeText = (EditText) findViewById(R.id.codeText);
+        verifyButton = (Button) findViewById(R.id.verifyButton);
+        sendButton = (Button) findViewById(R.id.sendButton);
+        resendButton = (Button) findViewById(R.id.resendButton);
+        signoutButton = (Button) findViewById(R.id.signoutButton);
         statusText = (TextView) findViewById(R.id.statusText);
-        emailText = (EditText) findViewById(R.id.emailText);
-        passwordText = (EditText) findViewById(R.id.passwordText);
-        userText.setText("");
+
+        verifyButton.setEnabled(false);
+        resendButton.setEnabled(false);
+        signoutButton.setEnabled(false);
         statusText.setText("Signed Out");
 
         fbAuth = FirebaseAuth.getInstance();
 
-        authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if (user != null) {
-                    userText.setText(user.getEmail());
-                    statusText.setText("Signed In");
-
-                } else {
-                    userText.setText("");
-                    statusText.setText("Signed Out");
-                }
-            }
-        };
 
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        fbAuth.addAuthStateListener(authListener);
+    public void sendCode(View view) {
+
+        String phoneNumber = phoneText.getText().toString();
+
+        setUpVerificatonCallbacks();
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                verificationCallbacks);
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (authListener != null) {
-            fbAuth.removeAuthStateListener(authListener);
-        }
-    }
+    private void setUpVerificatonCallbacks() {
 
-    public void createAccount(View view) {
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
+        verificationCallbacks =
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
-        if (email.length() == 0) {
-            emailText.setError("Enter an email address");
-            return;
-        }
-
-        if (password.length() < 6) {
-            passwordText.setError("Password must be at least 6 characters");
-            return;
-        }
-
-        fbAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this,
-                        new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                                if (!task.isSuccessful()) {
-                                    notifyUser("Account creation failed");
-                                }
-                            }
-                        });
-
-    }
-
-    public void signIn(View view) {
-
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-
-        if (email.length() == 0) {
-            emailText.setError("Enter an email address");
-            return;
-        }
-
-        if (password.length() < 6) {
-            passwordText.setError("Password must be at least 6 characters");
-            return;
-        }
-
-        fbAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this,
-                        new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull
-                                                           Task<AuthResult> task) {
-
-                                if (!task.isSuccessful()) {
-                                    notifyUser("Authentication failed");
-                                }
-                            }
-                        });
-
-    }
-
-    public void signOut(View view) {
-        fbAuth.signOut();
-    }
-
-    public void resetPassword(View view) {
-
-        String email = emailText.getText().toString();
-
-        if (email.length() == 0) {
-            emailText.setError("Enter an email address");
-            return;
-        }
-
-        fbAuth.sendPasswordResetEmail(email)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
+                    public void onVerificationCompleted(
+                            PhoneAuthCredential credential) {
+
+                        signoutButton.setEnabled(true);
+                        statusText.setText("Signed In");
+                        resendButton.setEnabled(false);
+                        verifyButton.setEnabled(false);
+                        codeText.setText("");
+                        signInWithPhoneAuthCredential(credential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+
+                        if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                            // Invalid request
+                            Log.d(TAG, "Invalid credential: "
+                                    + e.getLocalizedMessage());
+                        } else if (e instanceof FirebaseTooManyRequestsException) {
+                            // SMS quota exceeded
+                            Log.d(TAG, "SMS Quota exceeded.");
+                        }
+                    }
+
+                    @Override
+                    public void onCodeSent(String verificationId,
+                                           PhoneAuthProvider.ForceResendingToken token) {
+
+                        phoneVerificationId = verificationId;
+                        resendToken = token;
+
+                        verifyButton.setEnabled(true);
+                        sendButton.setEnabled(false);
+                        resendButton.setEnabled(true);
+                    }
+                };
+    }
+
+    public void verifyCode(View view) {
+
+        String code = codeText.getText().toString();
+
+        PhoneAuthCredential credential =
+                PhoneAuthProvider.getCredential(phoneVerificationId, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        fbAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            notifyUser("Reset email sent");
+                            signoutButton.setEnabled(true);
+                            codeText.setText("");
+                            statusText.setText("Signed In");
+                            resendButton.setEnabled(false);
+                            verifyButton.setEnabled(false);
+                            FirebaseUser user = task.getResult().getUser();
+
+                        } else {
+                            if (task.getException() instanceof
+                                    FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
                         }
                     }
                 });
     }
-    private void notifyUser(String message) {
-        Toast.makeText(MainActivity.this, message,
-                Toast.LENGTH_SHORT).show();
+
+    public void resendCode(View view) {
+
+        String phoneNumber = phoneText.getText().toString();
+
+        setUpVerificatonCallbacks();
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                verificationCallbacks,
+                resendToken);
     }
+
+    public void signOut(View view) {
+        fbAuth.signOut();
+        statusText.setText("Signed Out");
+        signoutButton.setEnabled(false);
+        sendButton.setEnabled(true);
+    }
+
 }
